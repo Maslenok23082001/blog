21fa8b235b668f66b519b2d2963c383c0ae43ebd
maslenok23082001 <mas2001h@mail.ru>
2017-04-25 01:26:04 +0800

    свёрстан html

---
+++ app/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../public/main.css">
+    <title></title>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 require_once "app/header.php";
+?>
+    <div id="main_menu">
+        <ul>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/">Главная</a></li>
+        </ul>
+    </div>
+    <content>
+        <div id="new_blogs">
+            <h1>Мой блог</h1>
+            <div id="blogs">
+                <div class="blog">
+                    <p><a href="#">название поста</a></p>
+                    <p><a href="#">категория</a></p>
+                    <div>текст поста</div>
+                </div>
+            </div>
+            <div id="comments">
+                <h3>Комментарии</h3>
+                <div class="comment">
+                    <p>автор комментария</p>
+                    <p>текст комментария</p>
+                </div>
+            </div>
+        </div>
+
+    </content>
 
 
+
+<?php
 require_once "app/footer.php";
 ?>
