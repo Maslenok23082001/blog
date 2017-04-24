c36a0b5d016bbdaa324a20fc96522554ebfbc5d0
maslenok23082001 <mas2001h@mail.ru>
2017-04-25 02:03:00 +0800

    свёрстан CSS

--- index.php
+++ index.php
@@ -11,18 +11,42 @@ require_once "app/header.php";
     <content>
         <div id="new_blogs">
             <h1>Мой блог</h1>
+            <hr>
             <div id="blogs">
                 <div class="blog">
-                    <p><a href="#">название поста</a></p>
-                    <p><a href="#">категория</a></p>
+                    <p class="tit"><a href="#">название поста</a></p>
+                    <p class="cat"><a href="#">категория</a></p>
+                    <div>текст поста</div>
+                </div>
+                <div class="blog">
+                    <p class="tit"><a href="#">название поста</a></p>
+                    <p class="cat"><a href="#">категория</a></p>
+                    <div>текст поста</div>
+                </div>
+                <div class="blog">
+                    <p class="tit"><a href="#">название поста</a></p>
+                    <p class="cat"><a href="#">категория</a></p>
+                    <div>текст поста</div>
+                </div>
+                <div class="blog">
+                    <p class="tit"><a href="#">название поста</a></p>
+                    <p class="cat"><a href="#">категория</a></p>
+                    <div>текст поста</div>
+                </div>
+                <div class="blog">
+                    <p class="tit"><a href="#">название поста</a></p>
+                    <p class="cat"><a href="#">категория</a></p>
                     <div>текст поста</div>
                 </div>
             </div>
             <div id="comments">
+
                 <h3>Комментарии</h3>
+
                 <div class="comment">
-                    <p>автор комментария</p>
+                    <p class="com_tit">автор комментария</p>
                     <p>текст комментария</p>
+                    <hr>
                 </div>
             </div>
         </div>
